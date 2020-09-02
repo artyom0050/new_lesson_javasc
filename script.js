78d083dd9343510b8197d119a9bab61ccbcab99c
Artyom <adyan.artyom@icloud.com>
2020-09-02 14:24:14 +0400

    add script file mod

--- script.js
+++ script.js
@@ -19,74 +19,71 @@ let appData = {
 	optionalExpenses: {},
 	income: [],
 	timeData: time,
-	savings: true
+	savings: true,
+	chooseExpenses: function () {
+		for (let i = 0; i < 2; i++) {
+			let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+				b = prompt("Во сколько обойдется?", '');
+		
+			if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null
+				&& a != '' && b != '' && a.length < 50) {
+					console.log('done');
+					appData.expenses[a] = b;
+				} else {
+					console.log("bad result");
+					i--;
+				}
+		
+		}
+	},
+	detectDayBudget: function () {
+		appData.moneyPerDay = (appData.budget / 30).toFixed();
+		alert('Бюджет на один день сщставляет'  + appData.moneyPerDay + "руб" );
+	},
+	detectLevel: function () {
+		if(appData.moneyPerDay < 100) {
+			console.log("Минимальный уровень достатка");
+		} else if (appData.moneyPerDay > 100 && appData.moneyPerDaym < 2000) {
+			console.log("Средний уровень достатка");
+		} else if(appData.moneyPerDay > 2000) {
+			console.log("Высокий уровень достатка");
+		} else {
+			console.log('Ошибочка ...!');
+		}
+	},
+	checkSavings: function () {
+		if (appData.savings == true) {
+			let save = +prompt("Какова сумма накоплений?"),
+				percent = +prompt("Под какой процент?");
+	
+				appData.monthIncome = save/100/12*percent;
+				alert("Доход с Вашего депозита в месяц: " + appData.monthIncome);
+		}
+	},
+	chooseOptExpenses: function () {
+		for (let i = 1; i < 3; i++) {
+			let questionOptExpenses = prompt("Статья необьязательных расходов?");
+			appData.optionalExpenses[i] = questionOptExpenses;
+			console.log(appData.optionalExpenses);
+		}
+	},
+	chooseIncome: function (){
+		let items = prompt("Что принесет дополнительный доход? (Перечислите через запятую)", '');
+
+		if (typeof(items) != 'string' || items == "" || typeof(items) == null) {
+			console.log('Вы ввели некоректные данные или не ввели их вовсе!');
+		} else {
+			appData.income = items.split(', ');
+			appData.income.push(promt('Может что-то еще?'));
+			appData.income.sort();
+		}
+
+		appData.income.forEach (function(itemmassive, i ) {
+			alert("Способы доп заработка: " + (i+1) +  " - " + itemmassive);
+		} );
+	}
 };
 
-
-
-function chooseExpenses() {
-	for (let i = 0; i < 2; i++) {
-		let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-			b = prompt("Во сколько обойдется?", '');
-	
-		if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null
-			&& a != '' && b != '' && a.length < 50) {
-				console.log('done');
-				appData.expenses[a] = b;
-			} else {
-				console.log("bad result");
-				i--;
-			}
-	
-	};
+for (let key in appData) {
+	console.log("Наша программа включает в себя данные: " + key + " - " + appData[key]);
 }
-chooseExpenses();
-
-function detectDayBudget() {
-	appData.moneyPerDay = (appData.budget / 30).toFixed();
-	alert('Бюджет на один день сщставляет'  + appData.moneyPerDay + "руб" );
-}
-detectDayBudget();
-
-function detectLevel() {
-	if(appData.moneyPerDay < 100) {
-		console.log("Минимальный уровень достатка");
-	} else if (appData.moneyPerDay > 100 && appData.moneyPerDaym < 2000) {
-		console.log("Средний уровень достатка");
-	} else if(appData.moneyPerDay > 2000) {
-		console.log("Высокий уровень достатка");
-	} else {
-		console.log('Ошибочка ...!');
-	}
-
-}
-detectLevel();
-
-function checkSavings() {
-	if (appData.savings == true) {
-		let save = +prompt("Какова сумма накоплений?"),
-			percent = +prompt("Под какой процент?");
-
-			appData.monthIncome = save/100/12*percent;
-			alert("Доход с Вашего депозита в месяц: " + appData.monthIncome);
-	}
-}
-checkSavings();
-
-function chooseOptExpenses() {
-	for (let i = 1; i < 3; i++) {
-		let questionOptExpenses = prompt("Статья необьязательных расходов?");
-		appData.optionalExpenses[i] = questionOptExpenses;
-		console.log(appData.optionalExpenses);
-	}
-}
-chooseOptExpenses();
-
-
-
-
-
-
-
-
-
